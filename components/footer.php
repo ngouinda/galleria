614be45123ff3ea910fd08e0186871d977f4a4b5
Aimé Ngouinda <juniorngouinda@gmail.com>
2025-12-07 07:38:42 +0100

    FEATURES: facto du code et mise en place du design

---
+++ components/footer.php
@@ -0,0 +1,20 @@
+<div id="layoutAuthentication_footer">
+                <footer class="py-4 bg-light mt-auto">
+                    <div class="container-fluid px-4">
+                        <div class="d-flex align-items-center justify-content-between small">
+                            <div class="text-muted">Copyright &copy; Your Website by Aimé NGOUINDA 2025</div>
+                            <div>
+                                <a href="#">Politique de confidentialité </a>
+                                &middot;
+                                <a href="#">Conditions d'utilisation &amp; CGU</a>
+                            </div>
+                        </div>
+                    </div>
+                </footer>
+            </div>
+        </div>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+        <script src="js/scripts.js"></script>
+        <!-- <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script> -->
+    </body>
+</html>
---
+++ components/head.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Gelleria</title>
+        <!-- Favicon-->
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+        
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-pIVpG5B+YJoPLR5Yw2p6RNPf8N5Hz9xCHjzq3xFz2cA0RduCMsZK0VvV0K1S1K6Dq+W8sELpAo0P5m0Q+Yx4g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+        <!-- Bootstrap icons-->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet" />
+        <!-- Core theme CSS (includes Bootstrap)-->
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
